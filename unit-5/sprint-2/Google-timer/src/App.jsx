e5ef59bfd7c9c99c02c083fe36be519d6825218b
sivagurubilli <gurusrija123@gmail.com>
2022-05-28 15:06:20 +0530

    useref

--- unit-5/sprint-2/Google-timer/src/App.jsx
+++ unit-5/sprint-2/Google-timer/src/App.jsx
@@ -3,12 +3,14 @@ import logo from './logo.svg'
 import './App.css'
 import Stopwatch from './components/Stopwatch'
 import Counter from './components/Counter'
+import Timer from './components/Timer'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
      <Counter />
+     <Timer />
     </div>
   )
 }
--- unit-5/sprint-2/Google-timer/src/components/Timer.jsx
+++ unit-5/sprint-2/Google-timer/src/components/Timer.jsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Timer = () => {
   return (
     <div>Timer
-
+timer dummy
 
 
     </div>
