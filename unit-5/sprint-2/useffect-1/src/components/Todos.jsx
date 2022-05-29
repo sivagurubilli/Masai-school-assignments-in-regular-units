df093f39c245b56b2ac109c14a9fa71a69406cdd
sivagurubilli <gurusrija123@gmail.com>
2022-05-29 14:53:51 +0530

    useffect

---
+++ unit-5/sprint-2/useffect-1/src/components/Todos.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from "axios"
+
+
+const Todo = () => {
+    const [count, setCount] = useState(0)
+    const [todos,settodos]=useState([])
+    const [totalcount,settotalcount] = useState(0)
+    const [newtodo,setnewtodo]=useState("")
+    const [page,setpage] = useState(1)
+    const [limit,setlimit] = useState(5)
+    
+    useEffect(()=>{
+      const gettodo =async()=>{
+          var d = await axios.get(`http://localhost:8080/todos?_page=${page}&_limit=${limit}`)
+     console.log(d.data)
+          settodos(d.data)
+     settotalcount(Number(d.headers['x-total-count']))
+        }
+        gettodo()
+       
+      },[page,limit])
+    
+    
+    
+     const settodo=()=>{
+    fetch('http://localhost:8080/todos',{
+      method:'POST',
+      headers:{
+    "content-type":"application/json"
+      },
+      body:JSON.stringify({
+         value:newtodo,
+         isCompleate:false
+      }),
+    }).then((r)=>r.json())
+    .then((d)=>{
+      settodos([...todos,d])
+      setnewtodo("")
+    })
+    
+     }
+    
+      return (
+        <>
+        <div className="App" onClick={()=>setCount(count+1)}>
+         
+    
+           <input value={newtodo} onChange={(e)=>setnewtodo(e.target.value)} />
+           <button onClick={()=>settodo()}>++</button>
+        </div>
+        {todos.map((e)=>{
+            return <h1 key ={e.id}>{e.id}::{e.value}</h1> 
+            })}
+                <button
+                disabled={page<=1}
+                onClick ={()=>{
+                  if(page>1){
+                    setpage(page-1)
+                  }
+                }}
+                >{"<"}</button>
+    
+        <button
+    disabled ={totalcount<page*limit}
+        onClick ={()=> setpage(page+1)}
+        >{">"}</button>
+    <select  onChange ={(e)=>setlimit(Number(e.target.value))}>
+    
+    <option value ={5}>5</option>
+      <option value ={10}>10</option>
+    
+      <option value ={20} >20</option>
+    
+    </select>
+    <input
+    type="number"
+    value={limit}
+    min={0}
+    max ={totalcount}
+    onChange ={(e)=>setlimit(Number(e.target.value))}
+    
+    />
+    
+    
+        </>
+      )
+}
+
+export default Todo
