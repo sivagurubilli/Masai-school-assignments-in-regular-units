ae50a2fbac0365b351be6993c50cf4788f2c9a56
sivagurubilli <gurusrija123@gmail.com>
2022-08-17 23:17:19 +0530

    ts

---
+++ rct-211-a/sprint-4/ts-as1/src/index.ts
@@ -0,0 +1,36 @@
+
+let name1: string= "siva"
+let age : number = 25
+
+let isFetching :boolean = false
+
+let nums :Array<number>
+nums = [1,2,3,4,5]
+
+
+let names :Array<string>
+names = ["siva","vinee","purna"]
+
+let todo :[string,boolean] = ["eating",false]
+
+
+
+enum  tech{
+    ueser,
+    superuser,
+    admin,superadmin
+}
+
+function Sum(x:number,y:number):number{
+    return x+y
+}
+
+
+function prod(a:number,b:number):number{
+    return a*b
+}
+
+function nam(name:string){
+    console.log(name)
+}
+nam("siva")
