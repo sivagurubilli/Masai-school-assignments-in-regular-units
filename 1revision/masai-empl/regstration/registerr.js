5d15672b323d42f88bb136ceddc5e401b3c1681f
sivagurubilli <gurusrija123@gmail.com>
2022-09-13 15:05:58 +0530

    register page

---
+++ 1revision/masai-empl/regstration/registerr.js
@@ -0,0 +1,25 @@
+
+
+
+document.querySelector(".button").addEventListener("click",()=>{
+let data ={
+    name:document.querySelector(".inputreg").value,
+    email:document.querySelector(".inputreg").value,
+    password:document.querySelector(".inputreg").value,
+    username:document.querySelector(".inputreg").value,
+    mobile:document.querySelector(".inputreg").value,
+    desc:document.querySelector(".inputregdsc").value,
+
+}
+
+const url=" https://masai-api-mocker.herokuapp.com//auth/register"
+const opt ={
+    method:"POST",
+    headers:{
+Accept :"application/json",
+"Content-Type":"application/json"
+    },
+    body:JSON.stringify(data)
+}
+fetch(url,opt).then((res)=>res.json).then((data)=>{console.log(data)})
+})
